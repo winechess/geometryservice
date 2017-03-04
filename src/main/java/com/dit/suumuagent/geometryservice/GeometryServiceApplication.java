e5abd58218dcb254ad5dec42751274782f5bf783
Sergey Vinichenko <winechess@gmail.com>
2017-03-04 20:51:54 +0300

    Initial commit

---
+++ src/main/java/com/dit/suumuagent/geometryservice/GeometryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dit.suumuagent.geometryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometryServiceApplication.class, args);
+	}
+}
